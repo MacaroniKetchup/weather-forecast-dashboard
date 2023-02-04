9f9cac22bc27dc40e2b708ed8450ea59df5a589d
MacKetchup <stylesthestyer@gmail.com>
2023-02-04 12:34:48 -0700

    attempting to display current weather on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,125 @@ var cityName = "";
 var today = dayjs();
 
 // Date and time Formatting for header
-var currentDate = today.format('MMMM D, YYYY h:mm:ss a ');
+var currentDate = today.format('MM/ D/ YYYY h:mm:ss a ');
 $("#currentDay").text(currentDate);
 
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-
 // Fetch request for weather based on city name
-function getWeather( city ) {
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+function getWeather(city) {
     var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk' + cityName + '&appid=' + APIkey;
 
     fetch(requestUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data)
-    })
-    .catch(function(){
-        // catch any errors
-    });
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+
+            // Display current weather
+            var currentWeatherEl = $('#currentWeather');
+            currentWeatherEl.addClass('border border-success');
+
+            // City Name and Display
+            var cityNameEl = ('<h2>');
+            cityNameEl.text(cityName);
+            currentWeatherEl.append(cityNameEl);
+
+            // Date
+            var cityNameDate = data.current.dt;
+            cityNameDate = today.unix(cityNameDate).format("MM/DD/YYYY");
+            var currentDateEl = $('<div>');
+            currentDateEl.text(`(${currentDate}) `);
+            cityNameEl.append(currentDateEl);
+
+            // Icon
+            var cityWeatherIcon = data.current.weather[0].icon;
+            var cityWeatherIconEl = $('<img>');
+            cityWeatherIconEl.attr("src", "http://openweathermap.org/img/wn/" + cityWeatherIcon + ".png")
+            cityNameEl.append(cityWeatherIconEl);
+
+            // Temp
+            var cityTemp = data.current.temp;
+            var cityTempEl = $('<p>')
+            cityTempEl.text(`T: ${cityTemp}°F`)
+            currentWeatherEl.append(cityTempEl);
+            // Wind
+            var cityWind = data.current.wind_speed;
+            var cityWindEl = $('<p>');
+            cityWindEl.text(`Wind: ${cityWind} MPH`);
+            currentWeatherEl.append(cityWindEl);
+            // Humidity
+            var cityHumid = data.current.humidity;
+            var cityHumidEl = $('<p>');
+            cityHumid.text(`Huimdity: ${cityHumid}`);
+            currentWeatherEl.append(cityHumidEl);
+
+        })
+        .catch(function () {
+            // catch any errors
+        });
+
+
 }
 
-window.onload = function(){
-    getWeather();
+window.onload = function () {
+    getWeather(cityName);
+
 }
+
+// ------------------------------------------------------------------------------------------------------
+// Display current weather
+// .then(function (data) {
+//     var currentWeatherEl = $('#currentWeather');
+//     currentWeatherEl.addClass('border border-success');
+
+//     // City Name and Display
+//     var cityNameEl = ('<h2>');
+//     cityNameEl.text(cityName);
+//     currentWeatherEl.append(cityNameEl);
+
+//     // Date
+//     var cityNameDate = data.current.dt;
+//     cityNameDate = today.unix(cityNameDate).format("MM/DD/YYYY");
+//     var currentDateEl = $('<div>');
+//     currentDateEl.text(`(${currentDate}) `);
+//     cityNameEl.append(currentDateEl);
+
+//     // Icon
+//     var cityWeatherIcon = data.current.weather[0].icon;
+//     var cityWeatherIconEl = $('<img>');
+//     cityWeatherIconEl.attr("src", "http://openweathermap.org/img/wn/" + cityWeatherIcon + ".png")
+//     cityNameEl.append(cityWeatherIconEl);
+
+//     // Temp
+//     var cityTemp = data.current.temp;
+//     var cityTempEl = $('<p>')
+//     cityTempEl.text(`T: ${cityTemp}°F`)
+//     currentWeatherEl.append(cityTempEl);
+//     // Wind
+//     var cityWind = data.current.wind_speed;
+//     var cityWindEl = $('<p>');
+//     cityWindEl.text(`Wind: ${cityWind} MPH`);
+//     currentWeatherEl.append(cityWindEl);
+//     // Humidity
+//     var cityHumid = data.current.humidity;
+//     var cityHumidEl = $('<p>');
+//     cityHumid.text(`Huimdity: ${cityHumid}`);
+//     currentWeatherEl.append(cityHumidEl);
+// })
+
+//----------------------------------------------------------------------------------------------------------
+//Storage
+// Click Function to search button on #searchBtn
+// $('#searchBtn').on('click', function(event) {
+//     event.preventDefault();
+
+//     cityName = $('#city-search').val();
+//     if (cityName === ''){
+//         return alert('Please Enter Valid City Name ');
+
+//     }
+//     getWeather(cityName);
+
+//     localStorage.setItem(cityName);
+// });
