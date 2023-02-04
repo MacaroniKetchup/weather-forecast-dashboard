28b2a13dbaa05826314e9152b4f3c0cd902a9aa9
MacKetchup <stylesthestyer@gmail.com>
2023-02-04 11:38:37 -0700

    API fetch is confirmed working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
-var APIkey = "63ac7b714560be67d7d8bb1554b4b6dd";
+var APIkey = "d9f33c7e6c4bc9f6d8b69aa6426ef777";
+var cityName = "";
+var today = dayjs();
+
+// Date and time Formatting for header
+var currentDate = today.format('MMMM D, YYYY h:mm:ss a ');
+$("#currentDay").text(currentDate);
 
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
 // Fetch request for weather based on city name
-function getWeather( cityName ) {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid={API key}' + APIkey;
+function getWeather( city ) {
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk' + cityName + '&appid=' + APIkey;
 
     fetch(requestUrl)
     .then(function (response) {
@@ -13,4 +19,11 @@ function getWeather( cityName ) {
     .then(function (data) {
         console.log(data)
     })
+    .catch(function(){
+        // catch any errors
+    });
+}
+
+window.onload = function(){
+    getWeather();
 }
